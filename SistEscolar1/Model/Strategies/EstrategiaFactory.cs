1c9b79a9616c409e07f349df3b0d31d32e98040c
dervalar <lisandro.mf@hotmail.com>
2026-05-19 03:10:59 -0300

    Guía 3 añadida con errores

---
+++ SistEscolar1/Model/Strategies/EstrategiaFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistEscolar1.Model.Strategies
+{
+    public static class EstrategiaFactory
+    {
+        public static IEstrategiaPromedio Crear(string nombre) => nombre switch
+        {
+            nameof(PromedioPonderado) => new PromedioPonderado(),
+            nameof(PromedioSinExtremos) => new PromedioSinExtremos(),
+            nameof(PromedioAprobacion) => new PromedioAprobacion(),
+            _ => new PromedioSimple(), // default
+        };
+    }
+}
---
+++ SistEscolar1/Repositorios/RepositorioMateriasJSON.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistEscolar1.Repositorios
+{
+    internal class RepositorioMateriasJSON
+    {
+    }
+}
