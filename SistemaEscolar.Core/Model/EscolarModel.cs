3a53c6634ce4de9b43fa15541ff660fd7eda18e7
dervalar <lisandro.mf@hotmail.com>
2026-06-10 15:38:28 -0300

    Arreglando errores de compilacion

--- SistemaEscolar.Core/Model/EscolarModel.cs
+++ SistemaEscolar.Core/Model/EscolarModel.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SistEscolar1.Controller;
-using SistEscolar1.View;
 
 
 namespace SistEscolar1.Model
