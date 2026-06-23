1adeb4fd653e6c119379cd50da159057e6102577
dervalar <lisandro.mf@hotmail.com>
2026-06-23 16:33:17 -0300

    Actualizado repomaterias

--- SistemaEscolar.Core/Repositorios/IRepositorioMaterias.cs
+++ SistemaEscolar.Core/Repositorios/IRepositorioMaterias.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SistEscolar1.Controller;
-using SistEscolar1.View;
+
 
 namespace SistEscolar1.Repositorios
 {
